8fc7e3a93e6d5f667f0875dd95186b5cdbccd388
Jakub Glapa <jakub.glapa@gmail.com>
2017-07-24 08:14:07 +0200

    upgrading to Grails3.2.11

--- gr3-perf-degradation/grails-app/conf/logback.groovy
+++ gr3-perf-degradation/grails-app/conf/logback.groovy
@@ -32,8 +32,6 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
 }
-else {
-    root(ERROR, ['STDOUT'])
-}
+
+root(ERROR, ['STDOUT'])
